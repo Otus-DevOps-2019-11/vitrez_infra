b7717bac3dd606522a809309f5c6c01b4a74b150
Vitaliy Reznikov <vitrez@gmail.com>
2020-01-19 20:30:39 +0300

    Intermediate result of HW9, page 45

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,8 @@
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+variable app_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-app-base"
+}
